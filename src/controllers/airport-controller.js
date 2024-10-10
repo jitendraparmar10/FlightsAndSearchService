e48a919399da3a10a2bd0b5cdee7b58136c80941
jitendraparmar10 <jitparmar993@gmail.com>
2024-10-10 19:49:11 +0530

    Added common crud classes for basic crud operations

---
+++ src/controllers/airport-controller.js
@@ -0,0 +1,33 @@
+const {AirportService} = require('../services/index');
+
+const airportService = new AirportService();
+
+const create = async (req,res)=> {
+    try {
+        const response = await airportService.create(req.body);
+        return res.status(201).json({
+            message:'Successfully create the airport',
+            err:{},
+            data: response,
+            success:true
+
+        })
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            data:{},
+            success:false,
+            err:error,
+            message:'Cannot create a new airport'
+        })
+    }
+}
+
+
+module.exports ={
+    create,
+    // destroy,
+    // get,
+    // getAll,
+    // update
+}
